97162fdd707247289bbe3c3a5e19a1749c9ac97e
Mustapha-Adegbite <adegbitemustapha247@gmail.com>
2026-06-27 23:19:50 +0100

    veluxe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Darling from "./components/Darling";
-import Showcase from "./components/Showcase";
-import Journey from "./components/Journey";
-import StayInTouch from "./components/StayInTouch";
-import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Darling from "../components/Darling";
+import Showcase from "../components/Showcase";
+import Journey from "../components/Journey";
+import StayInTouch from "../components/StayInTouch";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
